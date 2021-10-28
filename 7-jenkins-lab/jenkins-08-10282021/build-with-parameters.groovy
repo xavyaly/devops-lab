22d4cb19cb02ad4b45968f4471dc11a34b7d5a7a
Javed <wellboy.alam13@gmail.com>
2021-10-28 09:21:16 +0530

    10282021

--- 7-jenkins-lab/jenkins-08-10282021/build-with-parameters.groovy
+++ 7-jenkins-lab/jenkins-08-10282021/build-with-parameters.groovy
@@ -58,5 +58,137 @@ Password: SECRET
 Finished: SUCCESS
 
 ---------------------------------------------------------------------------------------------------
-    
+	
+// Global Timeout, Declarative Pipeline
 
+pipeline {
+    agent any
+    options {
+        timeout(time: 1, unit: 'HOURS') 
+    }
+    stages {
+        stage('Example') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+    }
+}
+
+O/P:
+
+Started by user xavyaly
+[Pipeline] Start of Pipeline
+[Pipeline] node
+Running on Jenkins in /var/lib/jenkins/workspace/declarative-pipeline
+[Pipeline] {
+[Pipeline] timeout
+Timeout set to expire in 1 hr 0 min
+[Pipeline] {
+[Pipeline] stage
+[Pipeline] { (Example)
+[Pipeline] echo
+Hello World
+[Pipeline] }
+[Pipeline] // stage
+[Pipeline] }
+[Pipeline] // timeout
+[Pipeline] }
+[Pipeline] // node
+[Pipeline] End of Pipeline
+Finished: SUCCESS
+
+---------------------------------------------------------------------------------------------------
+	
+//Stage Timeout, Declarative Pipeline
+
+pipeline {
+    agent any
+    stages {
+        stage('Example') {
+            options {
+                timeout(time: 1, unit: 'HOURS') 
+            }
+            steps {
+                echo 'Hello World'
+            }
+        }
+    }
+}
+
+O/P:
+	
+Started by user xavyaly
+[Pipeline] Start of Pipeline
+[Pipeline] node
+Running on Jenkins in /var/lib/jenkins/workspace/declarative-pipeline
+[Pipeline] {
+[Pipeline] stage
+[Pipeline] { (Example)
+[Pipeline] timeout
+Timeout set to expire in 1 hr 0 min
+[Pipeline] {
+[Pipeline] echo
+Hello World
+[Pipeline] }
+[Pipeline] // timeout
+[Pipeline] }
+[Pipeline] // stage
+[Pipeline] }
+[Pipeline] // node
+[Pipeline] End of Pipeline
+Finished: SUCCESS
+
+---------------------------------------------------------------------------------------------------
+	
+// clean workspace 
+// https://stackoverflow.com/questions/37468455/jenkins-pipeline-wipe-out-workspace
+	
+pipeline {
+    agent any
+    stages {
+        stage('Clean WorkSpace') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+    }
+    post { 
+        always { 
+            cleanWs()
+        }
+    }
+}
+
+O/P:
+
+Started by user xavyaly
+[Pipeline] Start of Pipeline
+[Pipeline] node
+Running on Jenkins in /var/lib/jenkins/workspace/declarative-pipeline
+[Pipeline] {
+[Pipeline] stage
+[Pipeline] { (Example)
+[Pipeline] echo
+Hello World
+[Pipeline] }
+[Pipeline] // stage
+[Pipeline] stage
+[Pipeline] { (Declarative: Post Actions)
+[Pipeline] cleanWs
+[WS-CLEANUP] Deleting project workspace...
+[WS-CLEANUP] Deferred wipeout is used...
+[WS-CLEANUP] done
+[Pipeline] }
+[Pipeline] // stage
+[Pipeline] }
+[Pipeline] // node
+[Pipeline] End of Pipeline
+Finished: SUCCESS
+
+---------------------------------------------------------------------------------------------------
+	
+How to take the jenkins backups ?
+	
+---------------------------------------------------------------------------------------------------
+	
